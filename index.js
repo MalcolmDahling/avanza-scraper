c523a87e97d0656d6d82e25b76b235f0931b8280
MalcolmDahling <mac.dahling@gmail.com>
2022-10-04 01:06:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
-  });
+});
 
 
 app.post('/', async(req, res) => {
