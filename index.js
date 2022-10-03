c869076caf70d0c2a3cda6b9536e43daa3de393e
MalcolmDahling <mac.dahling@gmail.com>
2022-10-04 00:08:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.listen(PORT, () => console.log('Server is running on port 3001.'));
+app.listen($PORT, () => console.log('Server is running on port 3001.'));
 
 
 app.post('/', async(req, res) => {
