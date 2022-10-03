26ad0a197bb948cf43df7aaf3b4f54a8a079c6c9
MalcolmDahling <mac.dahling@gmail.com>
2022-10-03 23:43:38 +0200

    changed to recieve post requests

--- index.js
+++ index.js
@@ -2,70 +2,69 @@ const express = require('express');
 const cors = require('cors');
 const puppeteer = require('puppeteer');
 
-const server = express();
-server.use(cors());
-server.listen(3001, () => console.log('Server is running on port 3001.'));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.listen(3001, () => console.log('Server is running on port 3001.'));
 
 
-server.get('/', async(req, res) => {
-    await pupp();
-    res.send(funds);
+app.post('/', async(req, res) => {
+    let data = await pupp(req.body.url);
+    res.send(data);
 });
 
 
-const funds = [
-    {url:'https://www.avanza.se/fonder/om-fonden.html/1236399/aktiespararna-smabolag-edge',            date:'', oneDay:'', oneMonth:'', threeMonths:'', thisYear:'', oneYear:''},
-    {url:'https://www.avanza.se/fonder/om-fonden.html/512559/handelsbanken-hallbar-energi-a1-sek',     date:'', oneDay:'', oneMonth:'', threeMonths:'', thisYear:'', oneYear:''},
-    {url:'https://www.avanza.se/fonder/om-fonden.html/551035/ikc-fastighetsfond-a',                    date:'', oneDay:'', oneMonth:'', threeMonths:'', thisYear:'', oneYear:''},
-    {url:'https://www.avanza.se/fonder/om-fonden.html/70331/ms-invf-us-growth-a-usd',                  date:'', oneDay:'', oneMonth:'', threeMonths:'', thisYear:'', oneYear:''},
-    {url:'https://www.avanza.se/fonder/om-fonden.html/1276725/odin-fastighet-c-sek',                   date:'', oneDay:'', oneMonth:'', threeMonths:'', thisYear:'', oneYear:''},
-    {url:'https://www.avanza.se/fonder/om-fonden.html/325406/spiltan-aktiefond-investmentbolag',       date:'', oneDay:'', oneMonth:'', threeMonths:'', thisYear:'', oneYear:''}
-];
 
-
-async function pupp(){
+async function pupp(url){
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    
-    
-    for(let i = 0; i < funds.length; i++){
-        await page.goto(funds[i].url, { waitUntil: 'networkidle0' });
 
-        //DATE
-        let date = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div.main-column.ng-tns-c294-5 > aza-card.ng-tns-c294-5.full-width-mobile > div.chart-development.ng-tns-c294-5.ng-star-inserted > section:nth-child(1) > div:nth-child(2) > span:nth-child(1)").outerHTML );
-        date = date.slice(69, 75);
-        funds[i].date = date;
-        //DATE
-
-        //ONE DAY
-        let oneDay = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div.main-column.ng-tns-c294-5 > aza-card.ng-tns-c294-5.full-width-mobile > div.chart-development.ng-tns-c294-5.ng-star-inserted > section:nth-child(1) > div:nth-child(2) > span:nth-child(2)").outerHTML );
-        funds[i].oneDay = getPercentage(oneDay);
-        //ONE DAY
-
-        //ONE MONTH
-        let oneMonth = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(1) > button > span:nth-child(2)").outerHTML );
-        funds[i].oneMonth = getPercentage(oneMonth);
-        //ONE MONTH
-
-        //THREE MONTHS
-        let threeMonths = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(2) > button > span:nth-child(2)").outerHTML );
-        funds[i].threeMonths = getPercentage(threeMonths);
-        //THREE MONTHS
-
-        //THIS YEAR
-        let thisYear = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)").outerHTML );
-        funds[i].thisYear = getPercentage(thisYear);
-        //THIS YEAR
-
-        //ONE YEAR
-        let oneYear = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)").outerHTML );
-        funds[i].oneYear = getPercentage(oneYear);
-        //ONE YEAR
+    let data = {
+        date: '',
+        oneDay: '',
+        oneMonth: '',
+        threeMonths: '',
+        oneYear: ''
     }
 
-    console.log(funds);
+    await page.goto(url, { waitUntil: 'networkidle0' });
+
+    //DATE
+    let date = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div.main-column.ng-tns-c294-5 > aza-card.ng-tns-c294-5.full-width-mobile > div.chart-development.ng-tns-c294-5.ng-star-inserted > section:nth-child(1) > div:nth-child(2) > span:nth-child(1)").outerHTML );
+    date = date.slice(69, 75);
+    data.date = date;
+    //DATE
+
+    //ONE DAY
+    let oneDay = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div.main-column.ng-tns-c294-5 > aza-card.ng-tns-c294-5.full-width-mobile > div.chart-development.ng-tns-c294-5.ng-star-inserted > section:nth-child(1) > div:nth-child(2) > span:nth-child(2)").outerHTML );
+    data.oneDay = getPercentage(oneDay);
+    //ONE DAY
+
+    //ONE MONTH
+    let oneMonth = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(1) > button > span:nth-child(2)").outerHTML );
+    data.oneMonth = getPercentage(oneMonth);
+    //ONE MONTH
+
+    //THREE MONTHS
+    let threeMonths = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(2) > button > span:nth-child(2)").outerHTML );
+    data.threeMonths = getPercentage(threeMonths);
+    //THREE MONTHS
+
+    //THIS YEAR
+    let thisYear = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)").outerHTML );
+    data.thisYear = getPercentage(thisYear);
+    //THIS YEAR
+
+    //ONE YEAR
+    let oneYear = await page.evaluate( () => document.querySelector("body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)").outerHTML );
+    data.oneYear = getPercentage(oneYear);
+    //ONE YEAR
+    
 
     await browser.close();
+
+    return data;
 }
 
 
