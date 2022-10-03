3a45e08e3f849b25c632e9bea10a16ded6ba428f
MalcolmDahling <mac.dahling@gmail.com>
2022-10-04 01:24:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ const puppeteer = require('puppeteer');
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST'],
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
-//cors
-// app.all('/*', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next();
-// });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 
 app.post('/', async(req, res) => {
