6be1abbd84d19a3364b8371975360f45bd126c76
MalcolmDahling <mac.dahling@gmail.com>
2022-10-07 20:23:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -93,4 +93,4 @@ function getPercentage(str){
 // PORT = process.env.PORT;
 // app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
 
-app.listen(3031, () => console.log(`Server is running on port 3030.`));
+app.listen(3030, () => console.log(`Server is running on port 3030.`));
