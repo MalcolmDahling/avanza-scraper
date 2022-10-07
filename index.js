d7854bb2f519b73d85615a0258631b944b3a2235
MalcolmDahling <mac.dahling@gmail.com>
2022-10-07 19:37:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,42 +36,44 @@ async function pupp(url){
     await page.goto(url, { waitUntil: 'networkidle0' });
 
     //NAME
-    let name = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.page-header.ng-tns-c294-5.ng-star-inserted > h1').outerHTML );
+    let name = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div:nth-child(1) > h1').outerHTML );
     name = name.slice(50, -13);
     data.name = name;
     //NAME
 
     //DATE
-    let date = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div.main-column.ng-tns-c294-5 > aza-card.ng-tns-c294-5.full-width-mobile > div.chart-development.ng-tns-c294-5.ng-star-inserted > section:nth-child(1) > div:nth-child(2) > span:nth-child(1)').outerHTML );
+    let date = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div:nth-child(2) > div > aza-card > div:nth-child(2) > section:nth-child(1) > div:nth-child(2) > span:nth-child(1)').outerHTML );
     date = date.slice(69, 75);
     data.date = date;
     //DATE
 
     //ONE DAY
-    let oneDay = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div.main-column.ng-tns-c294-5 > aza-card.ng-tns-c294-5.full-width-mobile > div.chart-development.ng-tns-c294-5.ng-star-inserted > section:nth-child(1) > div:nth-child(2) > span:nth-child(2)').outerHTML );
+    let oneDay = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div:nth-child(2) > div > aza-card > div:nth-child(2) > section:nth-child(1) > div:nth-child(2) > span:nth-child(2)').outerHTML );
     data.oneDay = getPercentage(oneDay);
     //ONE DAY
 
     //ONE MONTH
-    let oneMonth = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(1) > button > span:nth-child(2)').outerHTML );
+    let oneMonth = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div:nth-child(2) > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(1) > button > span:nth-child(2)').outerHTML );
     data.oneMonth = getPercentage(oneMonth);
     //ONE MONTH
 
     //THREE MONTHS
-    let threeMonths = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(2) > button > span:nth-child(2)').outerHTML );
+    let threeMonths = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div:nth-child(2) > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(2) > button > span:nth-child(2)').outerHTML );
     data.threeMonths = getPercentage(threeMonths);
     //THREE MONTHS
 
     //THIS YEAR
-    let thisYear = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)').outerHTML );
+    let thisYear = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div:nth-child(2) > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)').outerHTML );
     data.thisYear = getPercentage(thisYear);
     //THIS YEAR
 
     //ONE YEAR
-    let oneYear = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div.column-wrapper.ng-tns-c294-5 > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)').outerHTML );
+    let oneYear = await page.evaluate( () => document.querySelector('body > aza-app > aza-shell > div > main > div > aza-fund-guide > aza-subpage > div > div > div > div:nth-child(2) > div > aza-card > div.chart-wrapper > aza-area-chart > div > aza-period-picker > div > aza-period-button:nth-child(3) > button > span:nth-child(2)').outerHTML );
     data.oneYear = getPercentage(oneYear);
     //ONE YEAR
     
+
+    console.log(data);
 
     await browser.close();
 
